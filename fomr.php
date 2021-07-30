b84fef44594be653bdaa6c9e891aec0aa70914e1
ignacio <ortizignacio008@gmail.com>
2021-07-30 16:41:27 -0500

    guardar Login

---
+++ fomr.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<br><br>
+<fieldset>
+  <legend>LOGIN</legend>
+
+  <form action="insertarDatos.php" method="post" name="form">
+
+    <input type="text" name="nombre" id=""><br><br>
+    <input type="password" name="pw"><br><br>
+    <input type="submit" name="Insertar datos"><br><br>
+
+
+
+  </form>
+
+
+</fieldset>
+
+
+
+</body>
+</html>
